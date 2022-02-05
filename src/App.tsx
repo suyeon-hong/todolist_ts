0aabd04d579b5e17f04f70d101624de35b412cc9
suyeon-hong <bellahong95@gmail.com>
2022-02-05 11:56:22 +0900

    :sparkles: userInput 기능구현 완료
    userinput input값 받아와서 todolist 배열에 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,53 @@
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
 import "./App.css";
+import { IInput, IList } from "./interface";
 
 function App() {
+	const nextId = useRef(1);
+	const [userInput, setUserInput] = useState<IInput>({
+		task: "",
+		deadline: "",
+	});
+	const [todolist, setTodolist] = useState<IList[]>([]);
+	const { task, deadline } = userInput;
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+		const { name, value } = e.target;
+		setUserInput((input) => ({ ...input, [name]: value }));
+	};
+
+	const addTodo = (e: FormEvent<HTMLFormElement>): void => {
+		e.preventDefault();
+		if (task === "" || deadline === "") return;
+		setTodolist((todolist) =>
+			todolist.concat([{ id: nextId.current, ...userInput }])
+		);
+		setUserInput({
+			task: "",
+			deadline: "",
+		});
+		nextId.current++;
+		console.log(todolist);
+	};
+
 	return (
 		<div className="wrapper">
-			<form className="userInput">
+			<form className="userInput" onSubmit={addTodo}>
 				<div>
-					<input type="text" placeholder="할 일을 입력하세요" />
-					<input type="number" placeholder="마감기한을 입력하세요" />
+					<input
+						type="text"
+						name="task"
+						value={task}
+						onChange={handleChange}
+						placeholder="할 일을 입력하세요"
+					/>
+					<input
+						type="number"
+						name="deadline"
+						value={deadline}
+						onChange={handleChange}
+						placeholder="마감기한을 입력하세요"
+					/>
 				</div>
 				<button>등록</button>
 			</form>
---
+++ src/interface.ts
@@ -0,0 +1,8 @@
+export interface IInput {
+	task: string;
+	deadline: string;
+}
+
+export interface IList extends IInput {
+	id: number;
+}
