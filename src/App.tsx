1922655f9ba58c1d87137b11b14dc49c38d14f7f
suyeon-hong <bellahong95@gmail.com>
2022-02-05 11:29:36 +0900

    :lipstick: 레이아웃 구성 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-	return <div></div>;
+	return (
+		<div className="wrapper">
+			<form className="userInput">
+				<div>
+					<input type="text" placeholder="할 일을 입력하세요" />
+					<input type="number" placeholder="마감기한을 입력하세요" />
+				</div>
+				<button>등록</button>
+			</form>
+			<ul className="todolistContainer">
+				<li>
+					<span>할 일</span>
+					<span>1일 남음</span>
+					<button>X</button>
+				</li>
+			</ul>
+		</div>
+	);
 }
 
 export default App;
