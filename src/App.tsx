7b441a4bb28019f4f1eb10d5331b37a9ffc2e3b4
sueyon-hong <bellahong95@gmail.com>
2022-02-27 14:33:12 +0900

    :sparkles: Feat: datePicker 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,63 @@
-import { ChangeEvent, useState, useRef, useEffect } from "react";
+import _ from "lodash";
+
+import { ChangeEvent, useState, useRef, useEffect, FormEvent } from "react";
 import "./App.css";
-import { IInput, IList } from "interface";
+import { List } from "interface";
 import Todo from "components/Todo";
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { getYear, getMonth } from "date-fns";
+import ko from "date-fns/locale/ko";
 
 const TODOS = "todos";
 
 const App = () => {
-	const nextId = useRef(1);
-	const [input, setInput] = useState<IInput>({
-		task: "",
-		deadline: "",
-	});
+	const nextId = useRef(0);
+	const [input, setInput] = useState<string>("");
 	const [todoList, setTodoList] = useState<IList[]>([]);
+	const [deadline, setDeadline] = useState<Date | null>(new Date());
+
+	registerLocale("ko", ko);
+	const years = _.range(1990, getYear(new Date()) + 1, 1);
+	const months = [
+		"1월",
+		"2월",
+		"3월",
+		"4월",
+		"5월",
+		"6월",
+		"7월",
+		"8월",
+		"9월",
+		"10월",
+		"11월",
+		"12월",
+	];
 
 	const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
-		setInput((input) => ({ ...input, [e.target.name]: e.target.value }));
+		setInput(() => e.target.value);
 	};
 
-	const addTodo = (): void => {
-		if (input.task === "" || input.deadline === "") return;
-		setTodoList((list) => [...list, { id: nextId.current, ...input }]);
-		setInput(() => ({
-			task: "",
-			deadline: "",
-		}));
+	const addTodo = () => {
+		if (input === "") return;
+		const Month = deadline!.getMonth() + 1;
+		const Date = deadline?.getDate();
+
+		setTodoList((list) => [
+			...list,
+			{
+				id: nextId.current,
+				task: input,
+				deadline: `${Month}월 ${Date}일 까지`,
+			},
+		]);
+		setInput(() => "");
 		nextId.current += 1;
+	};
+
+	const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		addTodo();
 	};
 
 	const removeTodo = (id: number): void => {
@@ -37,6 +70,7 @@ const App = () => {
 		if (typeof savedList === "string") {
 			const parsedList = JSON.parse(savedList);
 			setTodoList(parsedList);
+			nextId.current = parsedList.length;
 		}
 	}, []);
 
@@ -46,27 +80,82 @@ const App = () => {
 
 	return (
 		<div className="todoContainer">
-			<div className="userInput">
+			<form className="userInput" onSubmit={submitHandler}>
 				<div>
 					<input
 						type="text"
 						name="task"
-						value={input.task}
+						value={input}
 						onChange={onChange}
 						placeholder="할 일을 입력하세요"
 					/>
-					<input
-						type="number"
-						name="deadline"
-						value={input.deadline}
-						onChange={onChange}
-						placeholder="마감기한을 입력하세요"
+					<DatePicker
+						renderCustomHeader={({
+							date,
+							changeYear,
+							changeMonth,
+							decreaseMonth,
+							increaseMonth,
+							prevMonthButtonDisabled,
+							nextMonthButtonDisabled,
+						}) => (
+							<div
+								style={{
+									margin: 10,
+									display: "flex",
+									justifyContent: "center",
+								}}
+							>
+								<button
+									onClick={decreaseMonth}
+									disabled={prevMonthButtonDisabled}
+								>
+									{"<"}
+								</button>
+								<select
+									value={getYear(date)}
+									onChange={({ target: { value } }) => changeYear(+value)}
+								>
+									{years.map((option: number) => (
+										<option key={option} value={option}>
+											{option}
+										</option>
+									))}
+								</select>
+
+								<select
+									value={months[getMonth(date)]}
+									onChange={({ target: { value } }) =>
+										changeMonth(months.indexOf(value))
+									}
+								>
+									{months.map((option) => (
+										<option key={option} value={option}>
+											{option}
+										</option>
+									))}
+								</select>
+
+								<button
+									onClick={increaseMonth}
+									disabled={nextMonthButtonDisabled}
+								>
+									{">"}
+								</button>
+							</div>
+						)}
+						selected={deadline}
+						dateFormat={"yyyy-MM-dd"}
+						locale={ko}
+						onChange={(date) => setDeadline(date)}
 					/>
 				</div>
-				<button onClick={addTodo}>등록</button>
-			</div>
+				<button className="addBtn" onClick={addTodo}>
+					등록
+				</button>
+			</form>
 			<div className="todolistContainer">
-				{todoList.map((list: IList) => (
+				{todoList.map((list: List) => (
 					<Todo list={list} key={list.id} removeTodo={removeTodo} />
 				))}
 			</div>
